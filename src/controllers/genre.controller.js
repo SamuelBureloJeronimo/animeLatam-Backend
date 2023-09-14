d6c5c02fdddf5343c58e924fdd55d977bb26255f
RodolfoPC98 <rodolfoprcr@outlook.com>
2023-09-13 23:52:47 -0600

    Genre schema finished

--- src/controllers/genre.controller.js
+++ src/controllers/genre.controller.js
@@ -1,22 +1,113 @@
 import Genre from "../schemas/genre.schema.js";
 
-let saveGenre = async(req, res) => {
+const saveGenre = async(req, res) => {
+    try {
+        const { name } = req.body;
+        const existingGenre = await Genre.findOne({ name });
+        if(existingGenre){
+            return res.status(400).json({ msg: "Name is already in use"});
+        }
 
-    let genre = new Genre();
-    let params = req.body;
-    genre.name = params.name;
+        const genre = new Genre({ name });
 
-let exist = await Genre.findOne({name: genre.name});
-if(exist){
-    res.json({msg: "Name is already in use"});
-} else {
-    await genre.save();
-    res.json({msg: 'Genre Saved', genre}).status(200);
+        await genre.save();
+
+        res.status(200).json({ msg: 'Genre Saved', genre });
+
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'There was a server error' });
+    }
 }
 
-    
+/*********************************************************************************/
+
+const getGenres = async(req, res) => {
+    try {
+        const genres = await Genre.find();
+        res.status(200).json(genres);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Internal server error' });
+    }
 }
+
+/*********************************************************************************/
+
+const getGenre = async (req, res) => {
+    try {
+        const genre = await Genre.findById(req.params.id);
+        //if not find the genre
+        if(!genre){
+            return res.status(404).json({ msg: 'Genre not found'});
+        }
+        res.status(200).json(genre);
+
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Internal server error' });
+    }
+}
+
+/*********************************************************************************/
+
+const updateGenre = async(req, res) => {
+    try {
+        const id = req.params.id;
+        const { name } = req.body;
+        const genre = await Genre.findById(id);
+
+        if(!genre){
+            return res.status(404).json({ msg: 'The genre to be updated does not exist'});
+        }
+
+        if(name && name !== genre.name){
+            const existingGenre = await Genre.findOne({ name });
+            if (existingGenre){
+                return res.status(400).json({ msg: `The ${name} genre already exists` });
+            }
+            genre.name = name;
+        }
+
+        const updatedGenre = await genre.save();
+
+        res.status(200).json({
+            msg: 'The genre was properly updated',
+            updated_data: updatedGenre,
+            changes: { name: genre.name }
+        });
+
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'There was an error on the server'});
+    }
+}
+
+/*********************************************************************************/
+
+const deleteGenre = async (req, res) => {
+    try {
+        const genre = await Genre.findById(req.params.id);
+
+        if(!genre){
+            return res.status(404).json({ msg: 'There is no genre to eliminate' });
+        }
+
+        const deletedGenre = await Genre.findByIdAndDelete(req.params.id);
+
+        res.status(200).json({ msg: 'Genre eliminated', deleted_genre: deletedGenre });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'There was a server error'});
+    }
+}
+
+/*********************************************************************************/
 
 export default{
-    saveGenre
+    saveGenre,
+    getGenres,
+    getGenre,
+    updateGenre,
+    deleteGenre
 }
--- src/routes/genre.routes.js
+++ src/routes/genre.routes.js
@@ -4,5 +4,9 @@ import genreController from '../controllers/genre.controller.js';
 const router = express.Router();
 
 router.post('/save-genre', genreController.saveGenre);
+router.get('/get-genres', genreController.getGenres);
+router.get('/get-genre/:id', genreController.getGenre);
+router.put('/update-genre/:id', genreController.updateGenre);
+router.delete('/delete-genre/:id', genreController.deleteGenre);
 
 export default router;
